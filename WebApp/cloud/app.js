483c489cea46e98569f1e17802696931c841339e
John-Franklin <johnfranklin42@gmail.com>
2015-10-03 16:37:46 -0700

    Angular works?
    Lesse if angular works or if I messed something up

--- WebApp/cloud/app.js
+++ WebApp/cloud/app.js
@@ -7,12 +7,16 @@
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
-
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/controllers'));
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
+app.get("/create", function(req,res){
+    res.status(200).sendFile(__dirname + '/views/joblist.html');
+})
 
 // // Example reading from the request query string of an HTTP get request.
 // app.get('/test', function(req, res) {
