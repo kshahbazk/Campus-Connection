cc7d04386f58f14d19e26b40400de8fe6167e0de
John-Franklin <johnfranklin42@gmail.com>
2015-10-07 00:37:10 -0700

    Added user profile
    created the user profile. profile.css is empty for now, but will
    eventually be used with this to add some color to the design. The
    design could use more work, but I’m done with it for now, as dealing
    with ejs to dynamically generate reviews ate up too much time.

--- WebApp/cloud/app.js
+++ WebApp/cloud/app.js
@@ -1,17 +1,22 @@
 
-
 // These two lines are required to initialize Express in Cloud Code.
  express = require('express');
  app = express();
-
+bodyParser = require('body-parser');
 // Global app configuration section
-app.set('views', 'cloud/views');  // Specify the folder to find templates
+//app.use('cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
-app.use(express.bodyParser());    // Middleware for reading request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));    // Middleware for reading request body
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
+});
+app.get('/profile', function(req, res) {
+    res.render('profile', { message: 'Congrats, you just set up your app!' });
 });
 
 // // Example reading from the request query string of an HTTP get request.
@@ -27,4 +32,4 @@ app.get('/hello', function(req, res) {
 // });
 
 // Attach the Express app to Cloud Code.
-app.listen();
+app.listen(8080);
