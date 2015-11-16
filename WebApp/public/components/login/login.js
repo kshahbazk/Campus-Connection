43942a447659420a20471626020a7fa991effb00
John-Franklin <johnfranklin42@gmail.com>
2015-11-16 10:14:56 -0800

    Fixed things. now you can define the title of a page! put it in the state call object as the property 'title'.
    At some point in the future, we might want to generate additions to MyApp.js and index.js based on directory names so we don't need to keep adding the new files we create to each. it would be pretty simple if we just add the title later; all the information is there.

--- WebApp/public/components/login/login.js
+++ WebApp/public/components/login/login.js
@@ -1,17 +1,16 @@
 /**
  * Created by johnfranklin on 10/15/15.
  */
-angular.module('MyApp').controller('LogIn', function(){
+angular.module('MyApp').controller('login', function($scope){
     //Modified from Parse's example
 
     console.log("BREATHING");
-    this.logIn = function() {
-        console.log(this);
+    $scope.logIn = function() {
         //user.email Notification? figure this out after we get connected.
 
         // other fields can be set just like with Parse.Object
 
-        Parse.User.logIn(this.username, this.password, {
+        Parse.User.logIn($scope.username, $scope.password, {
             success: function (user) {
                 // Hooray! Let them use the app now.
                 alert("IT WORKED");
---
+++ WebApp/public/components/product/productController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by johnfranklin on 11/16/15.
+ */
+angular.module("myApp").controller("productController", function($scope){
+
+})
