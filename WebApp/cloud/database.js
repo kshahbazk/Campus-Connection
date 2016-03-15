e2a853c485eae97fd233552e73d6e10ceb898e3c
John-Franklin <johnfranklin42@gmail.com>
2016-03-15 12:46:31 -0700

    Updated files to work with the ec2 database

--- WebApp/cloud/database.js
+++ WebApp/cloud/database.js
@@ -1,7 +1,7 @@
 
 var mongoose = require('mongoose');
 
-var url = process.env.MONGOLAB_URI || 'mongodb://localhost/campusconnection';
+var url = process.env.MONGOLAB_URI || 'mongodb://user:SlatfatfSsih2c2t@campusconnection.us:27017/campusconnection';
 mongoose.connect(url);
 console.log("Connected to MongoDB");
 module.exports = mongoose;
