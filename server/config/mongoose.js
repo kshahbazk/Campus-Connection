f4507ce4543e9a75a4c717ddc52065e9e8ca8bfe
John-Franklin <johnfranklin42@gmail.com>
2016-05-01 16:13:52 -0700

    Merged akDev so I can work on integrating search. resolved some merge conflicts.
    Added an option to run on local. just uncomment the line in mongoose.js

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,6 +1,7 @@
 mongoose = require('mongoose');
 
-var url = process.env.MONGOLAB_URI || 'mongodb://ccUser:SlatfatfSsih2c2t@campusconnection.us:27017/campusconnection';
+var url = //"mongodb://localhost:27017/campusconnection" ||
+    process.env.MONGOLAB_URI || 'mongodb://ccUser:SlatfatfSsih2c2t@campusconnection.us:27017/campusconnection';
 //'mongodb://localhost::27017/campusconnection';
 	//
 mongoose.connect(url);
