a25e25a3a3b093f7cf396078706452b4c7abae88
shahbazkhan <kshahbazk@yahoo.com>
2016-05-04 21:05:37 -0700

    Finished up code for PPV algorithm

--- ScalaAnalyticsEngine/src/main/scala/ScalaAnalyticsEngine.scala
+++ ScalaAnalyticsEngine/src/main/scala/ScalaAnalyticsEngine.scala
@@ -14,22 +14,39 @@ import org.apache.spark.sql._
 /** The Big Data engine behind the PPVCache */
 
 object ScalaAnalyticsEngine {
-
   def main(args: Array[String]) {
-
     // initiliaze all spark contexts
     val conf = new SparkConf().setAppName("ScalaAnalyticsEngine")
     val spark = new SparkContext(conf)
     val sqlContext = new SQLContext(spark)
-
-    val username = "ccUser"
+    // initialize all the authentication parameters
+    val user = "ccUser"
     val database = "campusconnection"
     val password = "SlatfatfSsih2c2t"
     val inTable = "ads"
     val outTable = "ppvCacheTest"
     val mongoDBhost = "campusconnection.us:27017"
-    val credentials = username + "," + database + "," + password 
-
+    val credentials = user + "," + database + "," + password
+    /** Pull in the ads table from MongoDB **/
+    val adsMongoConfig = MongodbConfigBuilder(Map(Host -> List("campusconnection.us:27017"), 
+        Credentials -> List(com.stratio.datasource.mongodb.config.MongodbCredentials(user, database, password.toCharArray)), 
+        Database -> "campusconnection", Collection ->"ads", SamplingRatio -> 1.0, WriteConcern -> "normal"))
+    val readConfig = adsMongoConfig.build()
+    val mongoRDD = sqlContext.fromMongoDB(readConfig)
+    /** Use the PPV caluclation algorithm on ads Dataframe **/
+    val orderbyPPVpointer = mongoRDD.groupBy("ppvPointer").agg("price" -> "avg")
+    // Rename values so they match the ppvcaches
+    val ppvCache = orderbyPPVpointer.withColumnRenamed("ppvPointer","_id").withColumnRenamed("avg(price)","ppv")
+    // Drop any rows with a null PPV pointer
+    val noNull = ppvCache.na.drop()
+    /** Prepare Dataframe to be written into MongoDB **/
+    val saveConfig = MongodbConfigBuilder(Map(Host -> List("campusconnection.us:27017"),
+     Credentials -> List(com.stratio.datasource.mongodb.config.MongodbCredentials(user, database, password.toCharArray)),
+      Database -> "campusconnection", Collection ->"ppvcaches", SamplingRatio -> 1.0, WriteConcern -> "normal", SplitSize -> 8,
+       SplitKey -> "_id"))
+    // Write our non-null Dataframe into MongoDB
+    noNull.saveToMongodb(saveConfig.build)
+    // Close the Spark Context when we are done.
     spark.stop()
   }
 }
